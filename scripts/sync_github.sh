ad1f329ae72f71fcb544841751c85b739e7005c7
skritch <sam.kritch@gmail.com>
2025-09-01 02:43:39 -0400

    Sync with master as of Mon Sep  1 02:43:39 EDT 2025: 2a5a421 Vibe-compiler proofreads

--- scripts/sync_github.sh
+++ scripts/sync_github.sh
@@ -3,4 +3,4 @@ git checkout github
 git checkout master -- .
 git add -A
 git commit -m "Sync with master as of $(date): $(git log -1 --oneline master)"
-# Then git push -u github github:master
+# Then git push -u github github:main
