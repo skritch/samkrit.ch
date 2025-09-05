4ea798c3df2e2dcf976dc6a2d61b67979ecc4c56
skritch <sam.kritch@gmail.com>
2025-09-05 17:15:11 -0400

    Sync: 92df817 Split out physics from dimensions post. Add TS examples to programming 1

--- scripts/sync_github.sh
+++ scripts/sync_github.sh
@@ -1,6 +1,9 @@
-
+# First:
+# git remote set-url --push github <github url>
+# git config remote.github.push refs/heads/github:refs/heads/main
 git checkout github
 git checkout master -- .
 git add -A
-git commit -m "Sync with master as of $(date): $(git log -1 --oneline master)"
-# Then git push -u github github:main
+git commit -m "Sync: $(git log -1 --oneline master)"
+# Then:
+# git push -u github github:main
